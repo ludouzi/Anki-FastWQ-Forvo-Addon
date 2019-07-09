c87b9e9e692cf9c3650dc4b0ab96ccf929be5335
Luke <45490980+luket9@users.noreply.github.com>
2019-07-09 14:13:53 +0100

    Language customization/small improvements

--- forvo.py
+++ forvo.py
@@ -12,7 +12,9 @@ class Forvo(WebService):
 
     def _get_from_api(self):
         # sorts by rating, so the highest rated audio file is downloaded.
-        url = 'https://apicorporate.forvo.com/api2/v1.2/d6a0d68b18fbcf26bcbb66ec20739492/word-pronunciations/word/{}/language/zh/order/rate-desc'.format(self.quote_word)
+        self.language = "zh"  # set language. Get language code from: https://forvo.com/languages-codes/
+        url = 'https://apicorporate.forvo.com/api2/v1.2/d6a0d68b18fbcf26bcbb66ec20739492/word-pronunciations/word/' \
+              '{}/language/' + self.language + '/order/rate-desc'.format(self.quote_word)
         html = self.get_response(url, timeout=10)
         result = {
             'mp3': u"",
@@ -28,6 +30,6 @@ class Forvo(WebService):
     @export([u'Voice', u'Voice'])
     def fld_mp3(self):
         url = self._get_field('mp3')
-        filename = u'forvo_{}.mp3'.format(self._word)
+        filename = u'forvo_{}.mp3'.format(self.word)
         if os.path.exists(filename) or self.download(url, filename):
             return self.get_anki_label(filename, 'audio')
